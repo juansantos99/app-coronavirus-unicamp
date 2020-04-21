dd7f759c5494512fa542073221bb6ff2b9e7d3b8
Wellington Silverio <wellington.alves.silverio@gmail.com>
2020-04-20 23:01:48 -0300

    Create ProfissionalDeSaude class

---
+++ src/main/java/copas/ProfissionalDeSaude.java
@@ -0,0 +1,77 @@
+
+package main.java.copas;
+
+public class ProfissionalDeSaude {
+    private int id;
+    private int rg;
+    private int cpf;
+    private String name;
+    private String role;
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getRg() {
+        return this.rg;
+    }
+
+    public void setRg(int rg) {
+        this.rg = rg;
+    }
+
+    public int getCpf() {
+        return this.cpf;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+
+    public int getName() {
+        return this.name;
+    }
+
+    public void setName(int name) {
+        this.name = name;
+    }
+
+    public int getRole() {
+        return this.role;
+    }
+
+    public void setRole(int role) {
+        this.role = role;
+    }
+
+    public void cadastrar() {
+    }
+
+    public void consultar() {
+    }
+
+    public void visualizarPaciente() {
+    }
+
+    public void consultarAgenda() {
+    }
+
+    public void agendarVisita() {
+    }
+
+    public void baixarVisita() {
+    }
+
+    public void registrarPontuario() {
+    }
+
+    public void atualizarStatus() {
+    }
+
+    public void contatarPaciente() {
+    }
+}
