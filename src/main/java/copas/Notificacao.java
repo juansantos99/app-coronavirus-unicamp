2b4e719aa8e3b2b8c05000c058adeac5a7817b47
Wellington Silverio <wellington.alves.silverio@gmail.com>
2020-04-20 22:43:15 -0300

    Create Notificacao class

---
+++ src/main/java/copas/Notificacao.java
@@ -0,0 +1,79 @@
+package main.java.copas;
+
+import java.util.Date;
+
+public class Notificacao {
+    private int id;
+    private String notification;
+    private String message;
+    private Date scheduling;
+    private String recipientName;
+    private String schedulingStatus;
+    private String mailerName;
+    private Date senderAt;
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNotification() {
+        return this.notification;
+    }
+
+    public void setNotification(String notification) {
+        this.notification = notification;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Date getScheduling() {
+        return this.scheduling;
+    }
+
+    public void setScheduling(Date scheduling) {
+        this.scheduling = scheduling;
+    }
+
+    public String getRecipientName() {
+        return this.recipientName;
+    }
+
+    public void setRecipientName(String recipientName) {
+        this.recipientName = recipientName;
+    }
+
+    public String getSchedulingStatus() {
+        return this.schedulingStatus;
+    }
+
+    public void setSchedulingStatus(String schedulingStatus) {
+        this.schedulingStatus = schedulingStatus;
+    }
+
+    public String getMailerName() {
+        return this.mailerName;
+    }
+
+    public void setMailerName(String mailerName) {
+        this.mailerName = mailerName;
+    }
+
+    public Date getSenderAt() {
+        return this.senderAt;
+    }
+
+    public void setSenderAt(Date senderAt) {
+        this.senderAt = senderAt;
+    }
+
+}
--- src/main/java/copas/Sintomas.java
+++ src/main/java/copas/Sintomas.java
@@ -1,3 +1,4 @@
+package main.java.copas;
 
 public class Sintomas {
     private int id;
