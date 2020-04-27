a6a17975fba087abf3f260b30dd9f1628ec581c5
Gabriel Santana <gabriel.gsantana7@gmail.com>
2020-04-26 21:10:21 -0300

    Add MedicalReport signin method

--- src/main/java/database/DBMedicalRecord.java
+++ src/main/java/database/DBMedicalRecord.java
@@ -1,5 +1,6 @@
 package main.java.database;
 
+import java.sql.Date;
 import main.java.database.DBConnection;
 
 import java.sql.Connection;
@@ -45,11 +46,51 @@ public class DBMedicalRecord {
 			
 			res = select.executeQuery();
 			
-			document = new MedicalRecord(res.getDate("RECORD_DATE"), res.getString("STATUS"), res.getInt("PATIENT_CPF"), res.getInt("DOCTOR_ID"), res.getString("DIAGNOSIS"), res.getString("EXAMS_ID"));
+			document = new MedicalRecord(res.getDate("RECORD_DATE"), res.getString("STATUS"), res.getInt("PATIENT_CPF"), res.getInt("DOCTOR_ID"), res.getString("DIAGNOSIS"), res.getString("EXAM_ID"));
 			
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+		
+		return document;
+	}
+	
+	public HealthProfessional SignIn(Date date, String status, int patientCpf, int doctor_id, String diagnosis, int exam_id) {		
+		
+		int generatedId = 0;
+		
+		try {
+			PreparedStatement statement = this.connection.prepareStatement("INSERT INTO MEDICAL_RECORD(ID, STATUS, RECORD_DATE, PATIENT_CPF, DOCTOR_ID, DIAGNOSIS, EXAM_ID) VALUES(?,?,?,?)", Statement.RETURN_GENERATED_KEYS);
+      
+      statement.setInt(1,  cpf);
+			statement.setString(2, status);
+			statement.setDate(3, date);
+      statement.setInt(4, patientCpf);
+      statement.setInt(5, doctor_id);
+      statement.setString(6, diagnosis);
+      statement.setInt(7, exam_id);
+			
+	        int affectedRows = statement.executeUpdate();
+
+	        if (affectedRows == 0) {
+	            throw new SQLException("Creating medical report failed, no rows affected.");
+	        }
+			
+			ResultSet generatedKeys = statement.getGeneratedKeys();
+			
+            if (generatedKeys.next()) {
+                generatedId = (int) generatedKeys.getLong(1);
+            }
+            else {
+                throw new SQLException("Creating medical report failed, no ID obtained.");
+            }
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		HealthProfessional document = new MedicalRecord(date, status, patientCpf, doctor_id, diagnosis, exam_id);
+		
+		System.out.println("Medical report successfully created! ID: " + generatedId);
 		
 		return document;
 	}
