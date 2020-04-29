a2dfc72543888461d40132b84f7e39d05fdd0e22
juansantos99 <juan-csantos@hotmail.com>
2020-04-29 14:18:38 -0300

    Classe Appointment e DBMedicalAppointment

--- src/main/java/database/DBMedicalAppointment.java
+++ src/main/java/database/DBMedicalAppointment.java
@@ -6,11 +6,8 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-
 import main.java.copas.Appointment;
-import main.java.copas.HealthProfessional;
-import main.java.copas.Symptons;
+
 
 public class DBMedicalAppointment {
 
@@ -31,7 +28,7 @@ public class DBMedicalAppointment {
 					"insert into MEDICAL_APPOINTMENT(DATE,PACIENT_CPF,HEALTHPROFESSIONAL_ID) values(?,?,?)");
 
 			select.setDate(1, (Date) dateAppointment);
-			select.setInt(2, cpf);
+			select.setLong(2,cpf);
 			select.setInt(3, idHealthProfissional);
 
 			select.executeUpdate();
@@ -65,10 +62,10 @@ public class DBMedicalAppointment {
 			select.executeUpdate();
 
 			while (res.next()) {
-				Appointment a = new Appointment();
-				a.setCpf(res.getInt(1));
-				a.setIdHealthProfissional(res.getInt(2));
-				listAppointment[listAppointment.length] = a;
+				Appointment appoint = new Appointment();
+				appoint.setPatientCpf((long)res.getInt(1));
+				appoint.setDoctorId(res.getInt(2));
+				listAppointment[listAppointment.length] = appoint;
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
